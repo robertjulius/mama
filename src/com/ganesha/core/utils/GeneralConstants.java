9bb1eec106e0bdd86c4a364e0a774c92c7ce4389
Robert Julius <robertjulius@gmail.com>
2014-02-06 01:36:35 +0700

    Prepare version v1.0.3

--- src/com/ganesha/core/utils/GeneralConstants.java
+++ src/com/ganesha/core/utils/GeneralConstants.java
@@ -11,7 +11,7 @@ import com.ganesha.desktop.component.ComboBoxObject;
 
 public class GeneralConstants {
 
-	public static final String VERSION = "v1.0.2";
+	public static final String VERSION = "v1.0.3";
 
 	public static final String USER_SESSION = "userSession";
 
