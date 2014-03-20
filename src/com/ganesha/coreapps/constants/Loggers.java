2ee6ea7bbe155499c7c882a5274e31355b766239
Robert Julius <robertjulius@gmail.com>
2014-03-20 08:55:52 +0700

    Merge setelah tambahin Logger untuk transasksi penjualan

--- src/com/ganesha/coreapps/constants/Loggers.java
+++ src/com/ganesha/coreapps/constants/Loggers.java
@@ -5,6 +5,6 @@ public class Loggers {
 	public static final String ROOT = "ROOT";
 	public static final String GLOBAL = "global";
 	public static final String APPLICATION = "application";
-	public static final String DB_ACCESS = "dbaccess";
+	public static final String SALE = "application.sale";
 
 }
