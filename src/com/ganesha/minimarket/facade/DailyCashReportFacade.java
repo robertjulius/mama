04e5c4aab659b0e0fefe04f7caaa683d8ed66fed
Robert Julius <robertjulius09@gmail.com>
2015-09-26 16:11:49 +0700

    Request fomr Sandy related to Daily Cash Report. He only need logs for transactions: - Sales - Sale Contraints - Sale Returns

--- src/com/ganesha/minimarket/facade/DailyCashReportFacade.java
+++ src/com/ganesha/minimarket/facade/DailyCashReportFacade.java
@@ -9,7 +9,6 @@ import java.util.Map;
 import org.hibernate.SQLQuery;
 import org.hibernate.Session;
 
-import com.ganesha.accounting.constants.CoaCodeConstants;
 import com.ganesha.core.utils.DateUtils;
 import com.ganesha.hibernate.HqlParameter;
 
@@ -27,33 +26,63 @@ public class DailyCashReportFacade {
 	private DailyCashReportFacade() {
 	}
 
-	public List<Map<String, Object>> search(Date beginDate, Date endDate,
-			Session session) {
+	public List<Map<String, Object>> search(Date beginDate, Date endDate, Session session) {
+
+		String newLine = "\r\n";
 
 		beginDate = DateUtils.validateDateBegin(beginDate);
 		endDate = DateUtils.validateDateEnd(endDate);
 
 		StringBuilder stringBuilder = new StringBuilder();
-		stringBuilder.append("SELECT CAST(timestamp AS DATE) AS DATE");
-		stringBuilder.append(", (SUM(debit) - SUM(credit)) AS amount");
-		stringBuilder.append(" FROM accounts");
-		stringBuilder.append(" WHERE 1=1");
-		stringBuilder.append(" AND coa_id = :coaId");
+		stringBuilder.append("SELECT ").append(newLine);
+		stringBuilder.append("     sh.date").append(newLine);
+		stringBuilder.append("      , IF (sh.amount IS NULL, 0, sh.amount)").append(newLine);
+		stringBuilder.append("           + IF (sch.amount IS NULL, 0, sch.amount)").append(newLine);
+		stringBuilder.append("           - IF (srh.amount IS NULL, 0, srh.amount)").append(newLine);
+		stringBuilder.append("           AS amount").append(newLine);
+		stringBuilder.append(" FROM (").append(newLine);
+		stringBuilder.append("      SELECT").append(newLine);
+		stringBuilder.append("           CAST(transaction_timestamp AS DATE) AS DATE").append(newLine);
+		stringBuilder.append("           , SUM(total_amount) AS amount").append(newLine);
+		stringBuilder.append("      FROM").append(newLine);
+		stringBuilder.append("           sale_headers").append(newLine);
+		stringBuilder.append("      GROUP BY").append(newLine);
+		stringBuilder.append("           date").append(newLine);
+		stringBuilder.append(" ) AS sh").append(newLine);
+		stringBuilder.append(" LEFT JOIN (").append(newLine);
+		stringBuilder.append("      SELECT").append(newLine);
+		stringBuilder.append("           CAST(transaction_timestamp AS DATE) AS DATE").append(newLine);
+		stringBuilder.append("           , SUM(total_amount) AS amount").append(newLine);
+		stringBuilder.append("      FROM").append(newLine);
+		stringBuilder.append("           sale_constraint_headers").append(newLine);
+		stringBuilder.append("      GROUP BY").append(newLine);
+		stringBuilder.append("           date").append(newLine);
+		stringBuilder.append(" ) AS sch ON sh.date = sch.date").append(newLine);
+		stringBuilder.append(" LEFT JOIN (").append(newLine);
+		stringBuilder.append("      SELECT").append(newLine);
+		stringBuilder.append("           CAST(transaction_timestamp AS DATE) AS DATE").append(newLine);
+		stringBuilder.append("           , SUM(total_return_amount) AS amount").append(newLine);
+		stringBuilder.append("      FROM").append(newLine);
+		stringBuilder.append("           sale_return_headers").append(newLine);
+		stringBuilder.append("      GROUP BY").append(newLine);
+		stringBuilder.append("           date").append(newLine);
+		stringBuilder.append(" ) srh ON sh.date = srh.date").append(newLine);
+		stringBuilder.append(" WHERE 1 = 1").append(newLine);
 
 		if (beginDate != null) {
-			stringBuilder.append(" AND timestamp >= :beginDate");
+			stringBuilder.append(" AND sh.date >= :beginDate ").append(newLine);
 		}
 
 		if (endDate != null) {
-			stringBuilder.append(" AND timestamp <= :endDate");
+			stringBuilder.append(" AND sh.date <= :endDate ").append(newLine);
 		}
 
-		stringBuilder.append(" GROUP BY date");
-		stringBuilder.append(" ORDER BY date DESC");
+		stringBuilder.append(" ORDER BY").append(newLine);
+		stringBuilder.append("      sh.date DESC;").append(newLine);
+		stringBuilder.append(" ").append(newLine);
 
 		SQLQuery sqlQuery = session.createSQLQuery(stringBuilder.toString());
 		HqlParameter parameter = new HqlParameter(sqlQuery);
-		parameter.put("coaId", CoaCodeConstants.KAS_KECIL);
 		parameter.put("beginDate", beginDate);
 		parameter.put("endDate", endDate);
 		parameter.validate();
